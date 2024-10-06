23082d77c9ddcb971bc9c843df1c75d01159551c
Sofie Olofsson <166747409+Sofski93@users.noreply.github.com>
2024-10-06 12:23:04 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exploreireland.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'explorefaroeislands.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

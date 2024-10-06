6fd8be9fa5490dc2ce17f68ee29a40bdff96543f
Sofie Olofsson <166747409+Sofski93@users.noreply.github.com>
2024-10-06 12:06:05 +0200

    Update asgi.py

--- explorefaroeislands/asgi.py
+++ explorefaroeislands/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'explorefaroeislands.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'explore_faroeislands.settings')
 
 application = get_asgi_application()

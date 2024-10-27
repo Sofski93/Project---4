7209ad554fcca8d225b7d64dfbac6470ed85905b
Sofie Olofsson <166747409+Sofski93@users.noreply.github.com>
2024-10-27 20:36:48 +0100

    Update settings.py

--- explorefaroeislands/settings.py
+++ explorefaroeislands/settings.py
@@ -43,11 +43,11 @@ LOGOUT_REDIRECT_URL = '/accounts/login'
 ACCOUNT_LOGOUT_REDIRECT_URL = '/'
 
 MESSAGE_TAGS = {
-    messages.DEBUG: 'alert-info',
-    messages.INFO: 'alert-info',
-    messages.SUCCESS: 'alert-success',
-    messages.WARNING: 'alert-warning',
-    messages.ERROR: 'alert-danger',
+    messages.DEBUG:'alert-info',
+    messages.INFO:'alert-info',
+    messages.SUCCESS:'alert-success',
+    messages.WARNING:'alert-warning',
+    messages.ERROR:'alert-danger',
 }
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
